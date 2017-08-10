32e108d86630e4e9e516335bcb6d5169218b36a1
anirudhavire <30728627+anirudhavire@users.noreply.github.com>
2017-08-10 18:26:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-var count=0;
-app.get('counter', function (req, res) {
+var counter=0;
+app.get('/counter', function (req, res) {
     counter=counter+1;
   res.send(counter);
 });
