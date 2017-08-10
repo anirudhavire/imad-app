ed5a6330ce3b716c48a35bb439cb7b53ea744f88
anirudhavire <30728627+anirudhavire@users.noreply.github.com>
2017-08-10 18:00:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,6 +17,10 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
 app.get('/one', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'bgcolour.html'));
 });
